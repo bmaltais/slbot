fdd5c207be10697876686693b5ce627acf8d4dee
dzaczek <dzaczek@sysop.cat>
2026-04-09 00:54:15 +0200

    karpathy: [tweak] S4/MASS_MANAGEMENT: length_bonus: 0.0171 -> 0.0191
    Round 8436, experiment 28d11420
    Strategy: tweak

--- styles.py
+++ styles.py
@@ -72,7 +72,7 @@ STYLES = {
                 "survival_escalation": 0.0007601423026215189,
                 "death_wall": -31.19617439894853,
                 "death_snake": -76.76737200950095,
-                "length_bonus": 0.01707087718500649,
+                "length_bonus": 0.019149485405332338,
                 "wall_proximity_penalty": 0.1515528096502023,
                 "enemy_alert_dist": 2086,
                 "enemy_proximity_penalty": 0.32140020176848344,
