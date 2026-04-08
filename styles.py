9d4420613009e8b08eb9122bccb8121cfe599ae3
dzaczek <dzaczek@sysop.cat>
2026-04-08 23:42:35 +0200

    karpathy: [explore] S1/FOOD_VECTOR: death_wall: -15.4063 -> -16.4078; survival: 0.0106 -> 0.0122; gamma: 0.8000 -> 0.8111
    Round 8109, experiment edca16d7
    Strategy: explore

--- styles.py
+++ styles.py
@@ -10,11 +10,11 @@ STYLES = {
         "stages": {
             1: {
                 "name": "FOOD_VECTOR",
-                "gamma": 0.8,
+                "gamma": 0.8111186507736082,
                 "food_reward": 1.8249151439002516,
                 "food_shaping": 0.018088474487101543,
-                "survival": 0.010601326513547447,
-                "death_wall": -15.406346747392735,
+                "survival": 0.012242625476068116,
+                "death_wall": -16.407836388580687,
                 "death_snake": -17.399026783495472,
                 "wall_proximity_penalty": 0.002240808950516122,
                 "max_steps": 106,
