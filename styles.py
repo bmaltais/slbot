9e37cb41755eed7fff8766a50cbb4acf3199bbd0
dzaczek <dzaczek@sysop.cat>
2026-04-09 00:48:32 +0200

    karpathy: [tweak] S2/WALL_AVOID: survival_escalation: 0.0001 -> 0.0001
    Round 8400, experiment df4b8168
    Strategy: tweak

--- styles.py
+++ styles.py
@@ -31,7 +31,7 @@ STYLES = {
                 "food_reward": 1.0,
                 "food_shaping": 0.07466764820325701,
                 "survival": 0.13448507732870238,
-                "survival_escalation": 5.7423506026446666e-05,
+                "survival_escalation": 6.808838286965299e-05,
                 "death_wall": -43.802797597124666,
                 "death_snake": -16.21870065987555,
                 "wall_alert_dist": 2500,
