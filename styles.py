56e7fcb00e85ecf19376323dff1a068854ec97dd
dzaczek <dzaczek@sysop.cat>
2026-04-09 01:05:52 +0200

    karpathy: [explore] S1/FOOD_VECTOR: max_steps: 113 -> 130; food_reward: 2.7776 -> 3.5809; death_wall: -14.0496 -> -15.2180
    Round 8494, experiment 58bd1284
    Strategy: explore

--- styles.py
+++ styles.py
@@ -11,13 +11,13 @@ STYLES = {
             1: {
                 "name": "FOOD_VECTOR",
                 "gamma": 0.9049091060510166,
-                "food_reward": 2.7776048851841666,
+                "food_reward": 3.5808805163832953,
                 "food_shaping": 0.0002667554306577553,
                 "survival": 0.039340712504713174,
-                "death_wall": -14.049582794709137,
+                "death_wall": -15.218025070283511,
                 "death_snake": -37.854450240329314,
                 "wall_proximity_penalty": 0.008126592439195583,
-                "max_steps": 113,
+                "max_steps": 130,
                 "promote_metric": "compound",
                 "promote_conditions": {
                     "avg_food": 12,
