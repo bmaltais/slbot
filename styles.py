bacc796c3f301961fd4277481cc376f98186f184
dzaczek <dzaczek@sysop.cat>
2026-04-09 00:22:28 +0200

    karpathy: [tweak] S1/FOOD_VECTOR: food_shaping: 0.0027 -> 0.0023
    Round 8286, experiment 43240661
    Strategy: tweak

--- styles.py
+++ styles.py
@@ -12,7 +12,7 @@ STYLES = {
                 "name": "FOOD_VECTOR",
                 "gamma": 0.9023317872147621,
                 "food_reward": 1.4944134201119128,
-                "food_shaping": 0.002707788536585805,
+                "food_shaping": 0.002333557865753986,
                 "survival": 0.01743189926402252,
                 "death_wall": -6.097221692725585,
                 "death_snake": -44.749386977702365,
