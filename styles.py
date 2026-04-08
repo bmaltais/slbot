cf72336e1b148071ac1b766c12eb10a18abcfb49
dzaczek <dzaczek@sysop.cat>
2026-04-08 22:43:56 +0200

    karpathy: [explore] S1/FOOD_VECTOR: wall_proximity_penalty: 0.0222 -> 0.0294; gamma: 0.8565 -> 0.8421; death_snake: -6.8243 -> -5.5922
    Round 7828, experiment bdeca8ca
    Strategy: explore

--- styles.py
+++ styles.py
@@ -10,13 +10,13 @@ STYLES = {
         "stages": {
             1: {
                 "name": "FOOD_VECTOR",
-                "gamma": 0.8565092558609175,
+                "gamma": 0.8420893225493775,
                 "food_reward": 1.0,
                 "food_shaping": 0.7091318071419344,
                 "survival": 0.018889554346097376,
                 "death_wall": -9.515348001174296,
-                "death_snake": -6.824251675937412,
-                "wall_proximity_penalty": 0.022224101023203723,
+                "death_snake": -5.5921843477331405,
+                "wall_proximity_penalty": 0.029384913196419928,
                 "max_steps": 105,
                 "promote_metric": "compound",
                 "promote_conditions": {
