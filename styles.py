8b29029ed2682c13c47120a2ffe556cf09d39380
dzaczek <dzaczek@sysop.cat>
2026-04-09 06:23:04 +0200

    karpathy: [tweak] S4/MASS_MANAGEMENT: boost_penalty: 0.0005 -> 0.0006
    Round 10082, experiment bc847fa6
    Strategy: tweak

--- styles.py
+++ styles.py
@@ -77,7 +77,7 @@ STYLES = {
                 "enemy_alert_dist": 1388,
                 "enemy_proximity_penalty": 0.04942497726690352,
                 "enemy_approach_penalty": 0.0010861328654893014,
-                "boost_penalty": 0.000530763940486811,
+                "boost_penalty": 0.0005827680372067078,
                 "mass_loss_penalty": 0.06505210488261112,
                 "starvation_penalty": 0.00010774927977767561,
                 "starvation_grace_steps": 10,
