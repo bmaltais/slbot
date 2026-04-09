78fa4fd447bf5d46225b5a9fc66537a672bef63f
dzaczek <dzaczek@sysop.cat>
2026-04-09 03:17:02 +0200

    karpathy: [radical] S1/FOOD_VECTOR: max_steps: 114 -> 119; death_wall: -9.5451 -> -10.7089; survival: 0.0341 -> 0.0427; food_shaping: 0.0000 -> 0.0000; food_reward: 1.5629 -> 1.8453
    Round 9085, experiment 76e049fd
    Strategy: radical

--- styles.py
+++ styles.py
@@ -11,13 +11,13 @@ STYLES = {
             1: {
                 "name": "FOOD_VECTOR",
                 "gamma": 0.835895989486164,
-                "food_reward": 1.5629013572010946,
-                "food_shaping": 1.3754175151550892e-05,
-                "survival": 0.03405543942084317,
-                "death_wall": -9.545118310737237,
+                "food_reward": 1.845257835881762,
+                "food_shaping": 2.1809823054348004e-05,
+                "survival": 0.04266605977212995,
+                "death_wall": -10.708908418725228,
                 "death_snake": -8.843865829299867,
                 "wall_proximity_penalty": 0.005388790611851052,
-                "max_steps": 114,
+                "max_steps": 119,
                 "promote_metric": "compound",
                 "promote_conditions": {
                     "avg_food": 12,
