4abd48c9de2c23b7a87cf0f8df1a35b59e49f348
dzaczek <dzaczek@sysop.cat>
2026-04-09 03:20:42 +0200

    karpathy: [tweak] S1/FOOD_VECTOR: death_snake: -8.8439 -> -7.8496
    Round 9100, experiment bd3ca490
    Strategy: tweak

--- styles.py
+++ styles.py
@@ -15,7 +15,7 @@ STYLES = {
                 "food_shaping": 2.1809823054348004e-05,
                 "survival": 0.04266605977212995,
                 "death_wall": -8.330610356502723,
-                "death_snake": -8.843865829299867,
+                "death_snake": -7.849635532505568,
                 "wall_proximity_penalty": 0.005388790611851052,
                 "max_steps": 119,
                 "promote_metric": "compound",
