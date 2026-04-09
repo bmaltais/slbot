72819d365a9e1d16b1e0a2c5554486ceb41d9e79
dzaczek <dzaczek@sysop.cat>
2026-04-09 02:10:18 +0200

    karpathy: [tweak] S1/FOOD_VECTOR: gamma: 0.9950 -> 0.9855
    Round 8849, experiment 38869898
    Strategy: tweak

--- styles.py
+++ styles.py
@@ -10,7 +10,7 @@ STYLES = {
         "stages": {
             1: {
                 "name": "FOOD_VECTOR",
-                "gamma": 0.995,
+                "gamma": 0.9854763547212255,
                 "food_reward": 1.0,
                 "food_shaping": 1.733799908326932e-05,
                 "survival": 0.01703386420092388,
